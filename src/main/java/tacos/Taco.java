aea39955bec57f53effeca14e586e354fba1bdf7
relentlesscoder <relentless.code@gmail.com>
2019-06-25 20:58:22 -0400

    authentication, jpa repostories

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -2,14 +2,18 @@ package tacos;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
 
 @Data
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private Date createdAt;
@@ -18,7 +22,13 @@ public class Taco {
     @Size(min=5, message = "Name must be at least 5 characters long")
     private String name;
 
+    @ManyToMany(targetEntity = Ingredient.class)
     @NotNull(message = "You must choose at least 1 ingredient")
     @Size(min = 1)
-    private List<String> ingredients;
+    private List<Ingredient> ingredients;
+
+    @PrePersist
+    void createdAt(){
+        this.createdAt = new Date();
+    }
 }
